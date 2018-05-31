01dc28ff151082323e1a0d9fdd15e797c078fa0e
McKamish <cory@ikick.rocks>
2018-05-30 18:25:56 -0700

    Finished the map lesson of the Udemy class. This is the final code.

--- map1.py
+++ map1.py
@@ -1,14 +1,33 @@
 import folium
+import pandas
+
+data = pandas.read_csv("Volcanoes_USA.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+elev = list(data["ELEV"])
+
+def color_producer(elevation):
+    if elevation < 1000:
+        return 'green'
+    elif 1000 <= elevation < 3000:
+        return 'orange'
+    else:
+        return 'red'
 map = folium.Map(location=[38.58,-99.09], zoom_start=6, tiles="Mapbox Bright")
 
-fg = folium.FeatureGroup(name="My Map")
+fgv = folium.FeatureGroup(name="Volcanoes")
 
-for coordinates in [[38.2, -99.1],[39.2, -97.1]]:
-    fg.add_child(folium.Marker(location=coordinates, popup="Hi I am a Marker", icon=folium.Icon(color='green')))
+for lt, ln, el in zip(lat, lon, elev):
+    fgv.add_child(folium.CircleMarker(location=[lt, ln], radius = 8, popup=str(el) + " meters",
+    fill = True, fill_color=color_producer(el) , color = 'grey', fill_opacity=0.7))   #in the video, 'fill' wasn't needed. Bt it is required for me....
 
-map.add_child(fg)
+fgp = folium.FeatureGroup(name="Population")
 
+fgp.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
+style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 1000000
+else 'orange' if 1000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
 
-
-
+map.add_child(fgv)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
 map.save("Map1.html")
